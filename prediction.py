a3a9f3fe38fbbb5919d519749f2dd20dc3ee85b3
yonathan <yonycherkos@gmail.com>
2020-01-08 06:43:29 +0300

    add encapsulated prediction module

--- prediction.py
+++ prediction.py
@@ -1,31 +1,48 @@
 import cv2
 import numpy as np
 from skimage.transform import resize
-from tensorflow.keras.models import load_model
+from tensorflow.keras.models import model_from_json
 from tensorflow.keras.preprocessing.image import img_to_array
 from tensorflow.keras import backend as kb
+from PIL import Image
 
-
-def get_model(model_path):
+# loading model architecture and weights
+def get_model(model_architecture_path, model_weights_path):
     global model
-    model = load_model(model_path)
+
+    # Model reconstruction from JSON file
+    with open(model_architecture_path, 'r') as f:
+        model = model_from_json(f.read())
+
+    # Load weights into the model
+    model.load_weights(model_weights_path)
     print(" * model loaded!")
 
-def transform_image(image, target_size = (224, 224)):
+def transform_image(image, target_size=(224, 224)):
     # image = PIL image
     image_array = np.asarray(image.convert("RGB"))
     image_array = image_array / 255.
     image_array = resize(image_array, target_size)
     return image_array
 
-def preprocess_image(image, target_size=(224, 224)):
-    # image = PIL image
-    if image.mode != "RGB":
-        image = image.convert("RGB")
-    image = image.resize(target_size)
-    image = img_to_array(image)
-    image = np.expand_dims(image, axis=0)
-    return image
+
+# def preprocess_image(image, target_size=(224, 224)):
+#     # image = PIL imageload_image
+#     if image.mode != "RGB":
+#         image = image.convert("RGB")
+#     image = image.resize(target_size)
+#     image = img_to_array(image) / 225.
+#     image = np.expand_dims(image, axis=0)
+#     return image
+
+# load the image
+def preprocess_image(image, target_size=(224, 224, 3)):
+    image_array = np.asarray(image.convert("RGB"))
+    image_array = image_array / 255.
+    image_array = np.resize(image_array, target_size)
+    image_tensor = np.expand_dims(image_array, axis=0)
+    return image_tensor
+
 
 def get_output_layer(model, layer_name):
     layer_dict = dict([(layer.name, layer) for layer in model.layers])
@@ -35,11 +52,14 @@ def get_output_layer(model, layer_name):
 
 def heatmap(model, orginal_image, transformed_image, predicted_class_index):
     # build tensorflow computational graph
-    class_weights = model.layers[-1].get_weights()[0]  # class_weight.shape = (1024, 14)
+    # class_weight.shape = (1024, 14)
+    class_weights = model.layers[-1].get_weights()[0]
     final_conv_layer = get_output_layer(model, "bn")
-    get_output = kb.function([model.layers[0].input], [final_conv_layer.output, model.layers[-1].output])
+    get_output = kb.function([model.layers[0].input], [
+                             final_conv_layer.output, model.layers[-1].output])
     [conv_outputs, predictions] = get_output([np.array([transformed_image])])
-    conv_outputs = conv_outputs[0, :, :, :]  # conv_outputs:  (7, 7, 1024) and predictions: (1, 14)
+    # conv_outputs:  (7, 7, 1024) and predictions: (1, 14)
+    conv_outputs = conv_outputs[0, :, :, :]
 
     # Create the class activation map.
     cam = np.zeros(dtype=np.float32, shape=(conv_outputs.shape[:2]))
@@ -57,6 +77,7 @@ def heatmap(model, orginal_image, transformed_image, predicted_class_index):
     cv2.imwrite("output/heatmap.jpg", heatmap)
     return heatmap
 
+
 def predict(model, image, target_size=(224, 224), show_heatmap=False):
     processed_image = preprocess_image(image, target_size)
     prediction = model.predict(processed_image).tolist()
@@ -64,7 +85,8 @@ def predict(model, image, target_size=(224, 224), show_heatmap=False):
         original_image = np.asarray(image.convert("RGB"))
         transformed_image = transform_image(image)
         predicted_class_index = np.argmax(prediction)
-        heatmap_img = heatmap(model, original_image, transformed_image, predicted_class_index)
+        heatmap_img = heatmap(model, original_image,
+                              transformed_image, predicted_class_index)
         return (prediction, heatmap_img)
     else:
         return prediction
